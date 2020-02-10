e51cff4ad4b9508d2f3d7b5ca49c915ba2592e41
Evan Wagstaff <evanjwagstaff@gmail.com>
2020-02-10 13:45:37 -0800

    Not optimized but it worked

--- root/tasks/less.js
+++ root/tasks/less.js
@@ -6,52 +6,37 @@ Run the LESS compiler against seed.less and output to style.css.
 
 module.exports = function(grunt) {
 
-  var postcss = require("postcss");
-  var autoprefixer = require("autoprefixer");
-  var cssnano = require("cssnano");
   var async = require("async");
   var less = require("less");
-
-  var path = require("path");
-  var through = require("through2");
-  var npmImporter = require("./lib/npm-less");
-
+  
   var options = {
     paths: ["src/css"],
-    plugins: [npmImporter],
+    filename: "seed.less"
   };
-
-  grunt.registerTask("less", "Compile styles from src/css/seed.less", function() {
-
+  
+  grunt.registerTask("less", function() {
+    
     var done = this.async();
 
-    var config = grunt.file.readJSON("project.json");
-
-    var seeds = config.styles;
+    var seeds = {
+      "src/css/seed.less": "build/style.css"
+    };
 
     async.forEachOf(seeds, function(dest, src, c) {
 
       var seed = grunt.file.read(src);
-
-      var o = Object.assign({}, options, { filename: seed });
-
-      less.render(seed, o, function(err, result) {
+      
+      less.render(seed, options, function(err, result) {
         if (err) {
           grunt.fail.fatal(err.message + " - " + err.filename + ":" + err.line);
         } else {
-          var styles = result.css;
-          postcss([autoprefixer, cssnano]).process(styles, {from: "undefined"}).then(result => {
-            result.warnings().forEach(warn => {
-              console.warn(warn.toString())
-            })
-            grunt.file.write(dest, result.css);
-          })
+          grunt.file.write(dest, result.css);
         }
         c();
       });
 
     }, done)
-
+    
   });
 
 };
